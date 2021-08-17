b24f137fe051e03d4b4e936a82b5f01ecfd1a37c
JaimeBotet <jaime.90.botet@gmail.com>
2021-08-17 10:56:08 +0200

    Add category functionality

--- routes/admin_categories.js
+++ routes/admin_categories.js
@@ -21,13 +21,9 @@ router.get('/', (req, res) => {
  */
 router.get('/add-category', (req, res) => {
     var title = "";
-    var slug = "";
-    var content = "";
 
     res.render('admin/add_category', {
-        title: title,
-        slug: slug,
-        content: content
+        title: title
     });
 });
 
@@ -37,44 +33,34 @@ router.get('/add-category', (req, res) => {
  router.post('/add-category', (req, res) => {
 
     req.checkBody('title', 'Title must have a value.').notEmpty();
-    req.checkBody('content', 'Content must have a value.').notEmpty();
 
     var title = req.body.title;
-    var content = req.body.content;
-    var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
-
-    if (slug == "") slug = title.replace(/\s+/g, '-').toLowerCase();
+    var slug = title.replace(/\s+/g, '-').toLowerCase();
 
     var errors = req.validationErrors();
 
     if (errors){
         res.render('admin/add_category', {
             errors: errors,
-            title: title,
-            slug: slug,
-            content: content
+            title: title
         });
     } else {
         Category.findOne({slug: slug}, (err, category) => {
             if (category) {
-                req.flash('danger', 'Category slug already exists, choose another...');
+                req.flash('danger', 'Category title already exists, choose another...');
                 res.render('admin/add_category', {
-                    title: title,
-                    slug: slug,
-                    content: content
+                    title: title
                 });
             } else {
                 let category = new Category({
                     title: title,
-                    slug: slug,
-                    content: content,
-                    sorting: 100
+                    slug: slug
                 });
                 category.save((err) => {
                     if(err) return console.log(err);
 
                     req.flash('success', 'Category added!');
-                    res.redirect('/admin/category');
+                    res.redirect('/admin/categories');
                 });
             }
         });
