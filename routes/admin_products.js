8c85faf120d03137a4b72b5ee1fd2dbbb559200c
JaimeBotet <jaime.90.botet@gmail.com>
2021-08-18 17:14:14 +0200

    POST add product

--- routes/admin_products.js
+++ routes/admin_products.js
@@ -51,42 +51,75 @@ router.get('/add-product', (req, res) => {
  */
  router.post('/add-product', (req, res) => {
 
+    var imageFile = typeof req.files.image !== "undefined"  ? req.files.image.name : "";
+
+
     req.checkBody('title', 'Title must have a value.').notEmpty();
-    req.checkBody('content', 'Content must have a value.').notEmpty();
+    req.checkBody('desc', 'Description must have a value.').notEmpty();
+    req.checkBody('price', 'Price must have a value.').isDecimal();
+    req.checkBody('image', 'You must upload an image.').isImage(imageFile);
 
     var title = req.body.title;
-    var content = req.body.content;
-    var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
-
-    if (slug == "") slug = title.replace(/\s+/g, '-').toLowerCase();
+    var desc = req.body.desc;
+    var price = req.body.price;
+    var category = req.body.category;
+    var slug = title.replace(/\s+/g, '-').toLowerCase();
 
     var errors = req.validationErrors();
 
+
+    
     if (errors){
-        res.render('admin/add_product', {
-            errors: errors,
-            title: title,
-            slug: slug,
-            content: content
+        Category.find( (err, categories) => {
+            res.render('admin/products/add_product', {
+                errors: errors,
+                title: title,
+                desc: desc,
+                categories: categories,
+                price: price
+            });
         });
     } else {
         Product.findOne({slug: slug}, (err, product) => {
             if (product) {
-                req.flash('danger', 'Product slug already exists, choose another...');
-                res.render('admin/add_product', {
-                    title: title,
-                    slug: slug,
-                    content: content
+                req.flash('danger', 'Product title already exists, choose another...');
+                Category.find( (err, categories) => {
+                    res.render('admin/products/add_product', {
+                        title: title,
+                        desc: desc,
+                        categories: categories,
+                        price: price
+                    });
                 });
             } else {
+                
+                var price2 = parseFloat(price).toFixed(2);
+
                 let product = new Product({
                     title: title,
                     slug: slug,
-                    content: content,
-                    sorting: 100
+                    desc: desc,
+                    price: price2,
+                    category: category,
+                    image: imageFile
                 });
+
                 product.save((err) => {
                     if(err) return console.log(err);
+                    
+                    // TODO the error is here, check it out before you continue
+                    mkdirp('/public/product_images/' + product._id, (err) => { return console.log('err1:' + err)});
+                    mkdirp('/public/product_images/' + product._id +'/gallery', (err) => { return console.log('err2:' + err)});
+                    mkdirp('/public/product_images/' + product._id + '/gallery/thumbs', (err) => { return console.log('err3:' + err)});
+
+
+                    //Move local image to public folder image
+                    if (imageFile != "") {
+                        var productImage = req.files.image;
+                        var public_path = '/public/product_images/' + product._id + '/' + imageFile;
+
+                        productImage.mv(public_path, (err) =>  console.log(err));
+                    }
 
                     req.flash('success', 'Product added!');
                     res.redirect('/admin/products');
